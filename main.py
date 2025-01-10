d2512f94cb95115223c385a4e1279b021f050604
jdra000 <juandavidrey@icloud.com>
2025-01-10 17:22:57 -0500

    done

--- main.py
+++ main.py
@@ -20,6 +20,22 @@ def binary_search(nums, target):
 print(binary_search([1,2,4,9,19,56,466,4567,7888], 56))
 
 
+# Selection Sort
+def selection_sort(nums):
+    for i in range(len(nums)):
+        smallest_idx = i
+
+        for j in range(smallest_idx+1, len(nums)):
+            if nums[j] < nums[smallest_idx]:
+                smallest_idx = j
+                
+        nums[i], nums[smallest_idx] = nums[smallest_idx], nums[i]
+
+    return nums
+
+
+
+
 # Merge Sort
 
 def merge_sort(nums):
@@ -62,6 +78,39 @@ def merge(left, right):
 
 print(merge_sort([1,77,4,56,24,34,56,212,9,2,1111]))
 
+
+
+# Insertion Sort
+def insertion_sort(nums):
+	for index in range(len(nums)):
+		j = index 
+		while j > 0 and nums[j-1] > nums[j]:
+			nums[j], nums[j-1] = nums[j-1], nums[j]
+			j-=1
+
+	return nums
+
+
+
+# Quick Sort
+def quick_sort(nums, low, high):
+	if low < high:
+		pivot = partition(nums, low, high)
+
+		quick_sort(nums, low, pivot - 1)
+		quick_sort(nums, pivot + 1, high)
+
+def partition(nums, low, high):
+	pivot = nums[high]
+	i = low
+
+	for j in range(low, high):
+		if nums[j] < pivot:
+			nums[i], nums[j] = nums[j], nums[i]
+			i+=1
+
+	nums[i], nums[high] = nums[high], nums[i]
+	return i
 
 
 
